6a9501c885f8f3c017db609f20134fea6cbdf3d3
Ariana-Amador <ariana@simplenight.com>
2023-02-01 08:47:27 -0400

    sort imports and fix java methods

--- binary_tree.java
+++ binary_tree.java
@@ -1,4 +1,7 @@
-public static class Node {
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
     int value;
     Node left;
     Node right;
@@ -10,68 +13,84 @@ public static class Node {
     }
 }
 
-Node a = new Node(1);
-Node b = new Node(2);
-Node c = new Node(3);
-Node d = new Node(4);
-Node e = new Node(5);
-Node f = new Node(6);
+class Tree {
+    Node root;
 
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
+    public Tree(Node root) {
+        this.root = root;
+    }
 
-public boolean breathFirstSearch(Node root, int target) {
-    if (root == null) {
+    public boolean breathFirstSearch(Node root, int target) {
+        if (root == null) {
+            return false;
+        }
+        Queue<Node> values = new LinkedList<>();
+        values.offer(root);
+        while (!values.isEmpty()) {
+            Node node = values.poll();
+            if (node.value == target) {
+                return true;
+            }
+            if (node.left != null) {
+                values.offer(node.left);
+            }
+            if (node.right != null) {
+                values.offer(node.right);
+            }
+        }
         return false;
     }
-    Queue<Node> values = new LinkedList<>();
-    values.offer(root);
-    while (!values.isEmpty()) {
-        Node node = values.poll();
-        if (node.value == target) {
+
+    public boolean depthFirstSearch(Node root, int target) {
+        if (root == null) {
+            return false;
+        }
+        if (root.value == target) {
             return true;
         }
-        if (node.left != null) {
-            values.offer(node.left);
+        return depthFirstSearch(root.left, target) || depthFirstSearch(root.right, target);
+    }
+
+    public int sumOfTree(Node root) {
+        if (root == null) {
+            return 0;
         }
-        if (node.right != null) {
-            values.offer(node.right);
+        return root.value + sumOfTree(root.left) + sumOfTree(root.right);
+    }
+
+    public int minValueOfTree(Node root) {
+        int smallest = Integer.MAX_VALUE;
+        if (root == null) {
+            return smallest;
         }
+        return Math.min(root.value, Math.min(minValueOfTree(root.left), minValueOfTree(root.right)));
     }
-    return false;
-}
 
-public boolean depthFirstSearch(Node root, int target) {
-    if (root == null) {
-        return false;
+    public int maxSumOfBranch(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        return root.value + Math.max(maxSumOfBranch(root.left), maxSumOfBranch(root.right));
     }
-    if (root.value == target) {
-        return true;
-    }
-    return depthFirstSearch(root.left, target) || depthFirstSearch(root.right, target);
-}
 
-public int sumOfTree(Node root) {
-    if (root == null) {
-        return 0;
-    }
-    return root.value + sumOfTree(root.left) + sumOfTree(root.right);
-}
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        Node f = new Node(6);
 
-public int minValueOfTree(Node root) {
-    int smallest = Integer.MAX_VALUE;
-    if (root == null) {
-        return smallest;
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.right = f;
+        Tree tree = new Tree(a);
+        System.out.println(tree.breathFirstSearch(tree.root, 5));
+        System.out.println(tree.depthFirstSearch(tree.root, 5));
+        System.out.println(tree.sumOfTree(tree.root));
+        System.out.println(tree.minValueOfTree(tree.root));
+        System.out.println(tree.maxSumOfBranch(tree.root));
     }
-    return Math.min(root.value, Math.min(minValueOfTree(root.left), minValueOfTree(root.right)));
-}
-
-public int maxSumOfBranch(Node root) {
-    if (root == null) {
-        return 0;
-    }
-    return root.value + Math.max(maxSumOfBranch(root.left), maxSumOfBranch(root.right));
 }
